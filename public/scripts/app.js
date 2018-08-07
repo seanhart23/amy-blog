8645a9f57a7d68c457589ea0ff308060bb49adb4
Sean Hart <seanhart23@gmail.com>
2018-08-07 02:32:45 +0000

    Fix date and add image and signature

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -54,53 +54,30 @@ function test2() {
     .appendTo('#slideshow2');
 }
 
-// var slideIndex = 0;
-// showDivs(slideIndex);
+function addtxt(input) {
+var obj=document.getElementById(input)
+obj.value += document.getElementById("textAreaNew").innerHTML = '<table class="imageFrame imageCaption"><tr><td><img border="0" height=“auto” src=“PUTURLHERE”><br></td></tr></table>'
+}
 
-// function plusDivs(n) {
-//     showDivs(slideIndex += n);
-//     showDivs1(slideIndex += n);
-//     showDivs2(slideIndex += n);
-// }
+function addtxtedit(input) {
+var obj=document.getElementById(input)
+obj.value += document.getElementById("textAreaEdit").innerHTML = '<table class="imageFrame imageCaption"><tr><td><img border="0" height=“auto” src=“PUTURLHERE”><br></td></tr></table>'
+}
 
-// function showDivs(n) {
-//     var i;
-//     var x = document.getElementsByClassName("mainImage");
-//     var y = document.getElementsByClassName("mainImageCaption");    
-//     if (n > x.length) {slideIndex = 1} 
-//     if (n < 1) {slideIndex = x.length} ;
-//     for (i = 0; i < x.length; i++) {
-//         x[i].style.display = "none"; 
-//         y[i].style.display = "none";         
-//     }
-//     x[slideIndex-1].style.display = "block"; 
-//     y[slideIndex-1].style.display = "block";     
-// }
+function addsig(input) {
+var obj=document.getElementById(input)
+obj.value += document.getElementById("textAreaNew").innerHTML = '<table class="imageFrame imageCaption"><tr><td><img border="0" src="http://www.harttohearts.com/images/xo.png" class="pull-right"></td></tr></table>'
+}
 
-// function showDivs1(n) {
-//     var i;
-//     var a = document.getElementsByClassName("mainImage1");
-//     var b = document.getElementsByClassName("mainImageCaption1");    
-//     if (n > a.length) {slideIndex = 2} 
-//     if (n < 1) {slideIndex = a.length} ;
-//     for (i = 0; i < a.length; i++) {
-//         a[i].style.display = "none"; 
-//         b[i].style.display = "none";         
-//     }
-//     a[slideIndex-1].style.display = "block"; 
-//     b[slideIndex-1].style.display = "block";     
-// }
+function addsigedit(input) {
+var obj=document.getElementById(input)
+obj.value += document.getElementById("textAreaEdit").innerHTML = '<table class="imageFrame imageCaption"><tr><td><img border="0" src="http://www.harttohearts.com/images/xo.png" class="pull-right"></td></tr></table>'
+}
 
-// function showDivs2(n) {
-//     var i;
-//     var c = document.getElementsByClassName("mainImage2");
-//     var d = document.getElementsByClassName("mainImageCaption2");    
-//     if (n > c.length) {slideIndex = 3} 
-//     if (n < 1) {slideIndex = c.length} ;
-//     for (i = 0; i < c.length; i++) {
-//         c[i].style.display = "none"; 
-//         d[i].style.display = "none";         
-//     }
-//     c[slideIndex-1].style.display = "block"; 
-//     d[slideIndex-1].style.display = "block";     
-// }
+$(".add").mouseup(function(){
+    $(this).blur();
+})
+
+$(".edit").mouseup(function(){
+    $(this).blur();
+})
